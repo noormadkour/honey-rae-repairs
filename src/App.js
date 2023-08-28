d79fc89b7d009e756ff70e430c0203b3facc538e
Noor Madkour <noormadkour@gmail.com>
2023-08-28 11:08:01 -0500

    added click button and reset button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import { useState } from "react"
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+  const [count, setCount] = useState(0)
+
+  const handleButtonClick = () => {
+    setCount(count + 1)
+  }
+
+  const resetCountButton = () => {
+    setCount(0)
+  }
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <div>Check out this cool clicker!</div>
+      <button className="btn-secondary" onClick={handleButtonClick}>Click me if you dare</button>
+      <div>Count: {count}</div>
+      <button className="btn-primary" onClick={resetCountButton}>Reset</button>
+    </>
+      )
 }
