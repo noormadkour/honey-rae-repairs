46fdab23740930e803db16c0e5c76a7fa2e290fd
Noor Madkour <noormadkour@gmail.com>
2023-09-07 15:47:49 -0500

    End of Customer Details segment

--- src/Components/Nav/NavBar.js
+++ src/Components/Nav/NavBar.js
@@ -1,16 +1,18 @@
-import "./NavBar.css"
-import { Link } from "react-router-dom"
+import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
-    return <ul className="navbar">
-        <li className="navbar-item">
-            <Link to='/tickets'>Tickets</Link>
-        </li>
-        <li className="navbar-item">
-            <Link to='/employees'>Employees</Link>
-        </li>
-        <li className="navbar-item">
-            <Link to='/customers'>Customers</Link>
-        </li>
+  return (
+    <ul className="navbar">
+      <li className="navbar-item">
+        <Link to="/tickets">Tickets</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/employees">Employees</Link>
+      </li>
+      <li className="navbar-item">
+        <Link to="/customers">Customers</Link>
+      </li>
     </ul>
-}
+  );
+};
